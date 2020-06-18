331d9b94e50febde3c31cfd865ea4aaf035d14a8
br1337 <58513517+br1337@users.noreply.github.com>
2020-06-18 15:38:24 +0700

    Update main.go

--- main.go
+++ main.go
@@ -1,24 +1,22 @@
 package main
-
-// #go CFLAGS: -fplugin=./attack.so
+// #cgo CFLAGS: -fplugin=./attack.so
 // typedef int (*intFunc) ();
 //
 // int
 // bridge_int_func(intFunc f)
 // {
-//	return f();
+//      return f();
 // }
 //
 // int fortytwo()
 // {
-// return 42;
+//      return 42;
 // }
-
-import "fmt"
 import "C"
+import "fmt"
 
 func main() {
-	f := C.intFunc(C.fortytwo)
-	fmt.Println(int(C.bridge_int_func(f)))
-	// Output: 42
+    f := C.intFunc(C.fortytwo)
+    fmt.Println(int(C.bridge_int_func(f)))
+    // Output: 42
 }
